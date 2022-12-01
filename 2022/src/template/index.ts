b0fc50f7adc84a7feb6922a68b189b01efb17900
Adriaan Baelus <adriaan@baelus.com>
2022-12-01 09:01:54 +0100

    Initial commit

---
+++ 2022/src/template/index.ts
@@ -0,0 +1,38 @@
+import run from 'aocrunner';
+
+const parseInput = (rawInput: string) => rawInput.split('\n').filter(Boolean);
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  trimTestInputs: true,
+  onlyTests: true,
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: '',
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: '',
+      // },
+    ],
+    solution: part2,
+  },
+});
